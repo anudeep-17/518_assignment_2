f7cb019b3746876ee3f6dd29e16a426dfd97da76
anudeep-17 <anudeepsai88@gmail.com>
2022-10-09 16:05:18 -0400

    edited

---
+++ source-code/my-client/src/body.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { render } from "react-dom";
+import { useState } from "react";
+
+export default function Body()
+{ 
+    const [formvalue, setFormValue] = useState({
+        name: "Sai vishnu anudeep kadiyala",
+        description : "hey guys, this is sai vishnu anudeep kadiyala, one of the students of ICSI 518, from team STUDY EASY. I am a combined bachelors and master student majoring in computer science, and a motivated athelte who loves to compete.",
+    });
+
+    const handleChange = (event) => {
+        const {name , value} = event.target;
+        setFormValue((prevState) => {
+            return {
+                ...prevState,
+                [name] : value,
+            }
+        });
+    };
+
+    const {name, description} = formvalue
+
+
+    return(
+    <>
+    
+    <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <img class="rounded float-left" src="https://www.myenglishteacher.eu/blog/wp-content/uploads/2018/01/ways-to-say-hello.jpg" width="300px" height="300px" />
+                </div>
+                <div class="col-sm">
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </div><div>
+                <br />
+                <h1> Enter information: </h1>
+            </div><div className="row d-block">
+                <div className="col">
+                    <form>
+                        <div className="form-floating">
+
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="form-control"
+                                value={name}
+                                onChange={handleChange} />
+                            <label htmlFor="name" for="name">Name</label>
+                        </div>
+                        <br />
+                        <div className="form-floating">
+
+                            <textarea
+                                type="text"
+                                name="description"
+                                id="description"
+                                className="form-control"
+                                onChange={handleChange}
+                                value={description} />
+                            <label htmlFor="description" for="description">Description</label>
+                        </div>
+                        <br />
+                        <div>
+                        </div>
+                    </form>
+                </div>
+            </div></>
+    );
+}
+
---
+++ source-code/my-client/src/start.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import 'bootstrap/dist/css/bootstrap.css';
+import Nav from './nav';
+import Body from './body';
+
+export default function Start() {
+  return (
+    <><div>
+                     <Nav />
+                    </div><div>
+                      <Body />
+                    </div></>
+  )
+}
